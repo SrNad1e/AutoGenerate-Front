e75fbe9dc5eb9d976adca3afe7fb9266deb38b1f
Camilo <camilob290421@gmail.com>
2022-04-05 07:49:01 -0500

    💥 feat(compiler): add estilizacion de la vista

--- src/pages/pos/new/index.tsx
+++ src/pages/pos/new/index.tsx
@@ -2,7 +2,7 @@ import {
   CheckCircleOutlined,
   DollarOutlined,
   PlusOutlined,
-  PrinterOutlined,
+  //PrinterOutlined,
   UserOutlined,
 } from '@ant-design/icons';
 import { Button, Card, Col, Popconfirm, Row, Tag, Tooltip, Typography } from 'antd';
@@ -12,14 +12,13 @@ const { Title, Text } = Typography;
 const PosList = () => {
   return (
     <Card
-      style={{ height: '92vh', border: 'solid 2px black', width: '40%', borderRadius: '1%' }}
+      style={{ height: '95vh', border: 'solid 2px black', width: '25%', borderRadius: '1%' }}
       bodyStyle={{ padding: 0, paddingTop: 5 }}
     >
       <Row
         style={{
           display: 'flex',
           flexDirection: 'row',
-          justifyContent: 'space-around',
           width: '100%',
         }}
       >
@@ -85,9 +84,9 @@ const PosList = () => {
         <Col
           span={24}
           style={{
-            marginBottom: 10,
             backgroundColor: 'floralwhite',
             borderBottom: 'solid 1px black',
+            height: '19vh',
           }}
         >
           <Row style={{ display: 'flex', alignItems: 'center', marginTop: 10 }}>
@@ -129,27 +128,22 @@ const PosList = () => {
               <Text style={{ fontSize: 18, paddingRight: 5 }}>{'$ 0'}</Text>
             </Col>
           </Row>
-          <Row style={{ marginBottom: 10, display: 'flex', alignItems: 'center' }}>
+          <Row style={{ display: 'flex', alignItems: 'center' }}>
             <Col
               span={12}
               style={{
                 display: 'flex',
                 alignItems: 'center',
-                height: '100%',
-                justifyContent: 'space-between',
               }}
             >
               <Title level={5} style={{ fontSize: 18, paddingLeft: 5 }}>
                 Envío:
               </Title>
             </Col>
-            <Col
-              span={12}
-              style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }}
-            >
+            <Col span={12} style={{ display: 'flex', justifyContent: 'right' }}>
               <Text style={{ fontSize: 18, paddingRight: 5 }}>{'N/A'}</Text>
             </Col>
-            <Col span={12} style={{ display: 'flex', justifyContent: 'center', paddingLeft: 250 }}>
+            <Col span={12} style={{ marginBottom: 10, display: 'flex', justifyContent: 'right' }}>
               <Button
                 shape="round"
                 icon={<PlusOutlined />}
@@ -162,32 +156,45 @@ const PosList = () => {
             </Col>
           </Row>
         </Col>
-        <Col span={24} style={{ backgroundColor: 'floralwhite' }}>
-          <Row style={{ marginTop: 5, paddingLeft: 20, backgroundColor: 'floralwhite' }}>
-            <Col span={12}>
+        <Col
+          span={24}
+          style={{
+            height: '9vh',
+            marginBottom: 20,
+            backgroundColor: 'floralwhite',
+          }}
+        >
+          <Row style={{ display: 'flex', alignItems: 'center' }}>
+            <Col span={12} style={{ display: 'flex', alignItems: 'center', paddingTop: 19 }}>
               <Button
                 shape="round"
                 icon={<DollarOutlined />}
                 type="primary"
-                style={{ fontSize: 25, padding: 15, width: 200, height: 'auto' }}
+                style={{ fontSize: 25, padding: 15, width: 'auto', height: 'auto' }}
                 onClick={() => {}}
               >
                 PAGAR
               </Button>
             </Col>
-            <Row gutter={[16, 16]} style={{ paddingLeft: 50, paddingTop: 20, margin: 'auto' }}>
-              <Col span={24}>
-                <Button
-                  shape="round"
-                  icon={<PrinterOutlined />}
-                  size="small"
-                  type="primary"
-                  style={{ backgroundColor: 'white', color: '#dc9575', width: 200 }}
-                >
-                  Imprimir
-                </Button>
-              </Col>
-            </Row>
+            <Col
+              span={12}
+              style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }}
+            >
+              <Text style={{ fontSize: 18, paddingRight: 5 }}>{'$ 2000'}</Text>
+            </Col>
+          </Row>
+          <Row style={{ display: 'flex', alignItems: 'center' }}>
+            <Col span={12} style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+              <Title level={5} style={{ fontSize: 18, paddingLeft: 5 }}>
+                Subtotal:
+              </Title>
+            </Col>
+            <Col
+              span={12}
+              style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }}
+            >
+              <Text style={{ fontSize: 18, paddingRight: 5 }}>{'$ 2000'}</Text>
+            </Col>
           </Row>
         </Col>
       </Row>
