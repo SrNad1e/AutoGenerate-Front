f4229146e9af4a463b56ad29f5ef1b0374b97c08
Darklegion <onlin1992@gmail.com>
2022-04-22 09:49:01 -0500

    💥 feat(compiler): Se realizan en mutation de warehouses

--- src/graphql/queries/warehouse.queries.ts
+++ src/graphql/queries/warehouse.queries.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const WAREHOUSES = gql`
-  query warehouses($input: FiltersWarehouseInput!) {
+  query warehouses($input: FiltersWarehousesInput!) {
     warehouses(filtersWarehouseInput: $input) {
       docs {
         _id
