b4dfddb799ec11a83d86a33b24bfe77e9c81015f
Darklegion <onlin1992@gmail.com>
2022-04-26 15:54:54 -0500

    💥 feat(compiler): Se realizan los cambios generales al api

--- config/config.pre.ts
+++ config/config.pre.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     'react-dev-inspector/plugins/umi/react-inspector',
   ],
   define: {
-    API_URL: 'http://localhost:8080/graphql',
-    CDN_URL: 'http://cdn.toulouse.com.co',
+    API_URL: 'http://localhost:8080',
+    CDN_URL: 'https://toulouse-storage.s3.amazonaws.com',
     COMPANY_ID: '62503250f035862fa3925f70',
     FORMAT_DATE: 'DD/MM/YYYY HH:mm:ss',
     FORMAT_DATE_API: 'YYYY/MM/DD',
