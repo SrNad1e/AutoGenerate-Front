1bfe562a9cc9f7805f899e36cd1d8a61b1a6de07
Camilo <camilob290421@gmail.com>
2022-05-16 14:46:31 -0500

    💥 feat(compiler): add se agrego el footer al formulario de creacion

--- src/pages/inventory/transfer/components/FormTransfer.tsx
+++ src/pages/inventory/transfer/components/FormTransfer.tsx
@@ -1,4 +1,18 @@
-import { Badge, Button, Card, Col, Descriptions, InputNumber, Row, Table, Tag } from 'antd';
+import {
+  Affix,
+  Badge,
+  Button,
+  Card,
+  Col,
+  Descriptions,
+  Divider,
+  InputNumber,
+  Row,
+  Space,
+  Table,
+  Tag,
+  Typography,
+} from 'antd';
 import { useModel } from 'umi';
 import type { ColumnsType } from 'antd/es/table/interface';
 import { CheckCircleOutlined, DeleteFilled } from '@ant-design/icons';
@@ -8,8 +22,9 @@ import { StatusType } from '../tranfer.data';
 import type { StockTransfer } from '@/graphql/graphql';
 import SearchProducts from '@/components/SearchProducts';
 
-import './styles.less';
+import styles from './styles.less';
 
+const { Title } = Typography;
 const DescriptionsItem = Descriptions.Item;
 
 export type Props = {
@@ -19,31 +34,27 @@ export type Props = {
 const FormTransfer = ({ transfer }: Props) => {
   const { initialState } = useModel('@@initialState');
 
-  const columns: ColumnsType<Partial<StockTransfer>> = [
+  const columns: ColumnsType = [
     {
       title: 'Producto',
       dataIndex: 'product.barcode',
       width: 250,
-      render: (barcode: string, record) => (
+      render: (barcode: string) => (
         <>
-          {`${record.details && record.details[0].product.reference} / ${
-            record.details && record.details[0].product.reference.description
-          }`}
+          {`${'Referencia'} / ${'Descripcion'}`}
           <Tag>{barcode}</Tag>
         </>
       ),
     },
     {
       title: 'Talla y Color',
-      dataIndex: 'product.barcode',
+      dataIndex: 'product',
       width: 200,
-      render: (_: string, product) => {
+      render: () => {
         return (
           <>
-            {product.details && product.details[0].product.size}
-            <Tag color={product.details && product?.details[0].product.color.html}>
-              {product.details && product?.details[0].product.color.name}
-            </Tag>
+            {'valor de la talla'}
+            <Tag color={'blue'}>{'nombre del color'}</Tag>
           </>
         );
       },
@@ -83,10 +94,10 @@ const FormTransfer = ({ transfer }: Props) => {
           <Col lg={12} xs={24}>
             <Descriptions bordered size="middle">
               <DescriptionsItem label="Bodega de destino" span={3}>
-                {transfer?.warehouseDestination?.name}
+                {'Bodega'}
               </DescriptionsItem>
               <DescriptionsItem label="Usuario que recibe" span={3}>
-                {transfer?.userDestination}
+                {'Usuario destinado'}
               </DescriptionsItem>
               <DescriptionsItem label="Observación de recepción">
                 {'Observacion a recibir'}
@@ -132,6 +143,42 @@ const FormTransfer = ({ transfer }: Props) => {
         <SearchProducts />
       </Card>
       <Table columns={columns} scroll={{ x: 1500 }} />
+      <Affix offsetBottom={0}>
+        <Card bordered={false}>
+          <Row justify="center" align="middle">
+            <Col span={4}>
+              <Button
+                disabled={false}
+                type={transfer?._id ? 'primary' : 'default'}
+                danger={!!transfer?._id}
+                onClick={() => {}}
+              >
+                Cancelar
+              </Button>
+            </Col>
+            <Col span={14}>
+              {' '}
+              <Space className={styles.centerFooter}>
+                <Title level={3}>
+                  REFERENCIAS: 0
+                  <Divider type="vertical" />
+                  PRODUCTOS: 0
+                </Title>
+              </Space>
+            </Col>
+            <Col span={6}>
+              <Space>
+                <Button disabled={false} onClick={() => {}}>
+                  Guardar
+                </Button>
+                <Button type="primary" disabled={false} onClick={() => {}}>
+                  Enviar
+                </Button>
+              </Space>
+            </Col>
+          </Row>
+        </Card>
+      </Affix>
     </>
   );
 };
