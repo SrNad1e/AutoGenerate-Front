adae638282ded37db4d69a47edced2249d62fef9
Camilo <camilob290421@gmail.com>
2022-05-17 17:00:06 -0500

    💥 feat(compiler): add se finalizo la interfaz de devoluciones (faltan funcionalidades)

--- src/pages/billing/return/list/index.tsx
+++ src/pages/billing/return/list/index.tsx
@@ -1,33 +1,138 @@
-import { PlusOutlined, SearchOutlined } from '@ant-design/icons';
+import { PlusOutlined, PrinterFilled, SearchOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Button, Card, Col, Divider, Form, Input, Row, Select, Space, Typography } from 'antd';
+import {
+  Button,
+  Card,
+  Col,
+  Divider,
+  Form,
+  Input,
+  Row,
+  Select,
+  Space,
+  Table,
+  Tooltip,
+  Typography,
+} from 'antd';
+import type { ColumnsType } from 'antd/es/table/interface';
+import moment from 'moment';
+import { useState } from 'react';
+
+import FormReturn from '../form';
+
+import styles from './styles';
 
 const FormItem = Form.Item;
 const { Option } = Select;
 const { Text } = Typography;
 
 const ReturnList = () => {
+  const [visible, setVisible] = useState(false);
+
+  const closeModal = () => {
+    setVisible(false);
+  };
+
+  const dataTest = [
+    {
+      code: '1',
+      invoice: 4321,
+      order: 32,
+      shop: '10 Downing Street',
+      value: 10000,
+    },
+    {
+      code: '2',
+      invoice: 1234,
+      order: 42,
+      shop: '10 Downing Street',
+      value: 10000,
+    },
+  ];
+
+  const columns: ColumnsType = [
+    {
+      title: 'Codigo',
+      dataIndex: 'code',
+      align: 'center',
+      sorter: true,
+      showSorterTooltip: false,
+    },
+    {
+      title: 'Pedido',
+      dataIndex: 'order',
+      align: 'center',
+    },
+    {
+      title: 'Factura',
+      dataIndex: 'invoice',
+      align: 'center',
+    },
+    {
+      title: 'Tienda',
+      dataIndex: 'shop',
+      align: 'center',
+      sorter: true,
+      showSorterTooltip: false,
+    },
+    {
+      title: 'Valor',
+      dataIndex: 'value',
+      align: 'center',
+      sorter: true,
+      showSorterTooltip: false,
+    },
+    {
+      title: 'Fecha de creación',
+      dataIndex: 'createdAt',
+      align: 'center',
+      sorter: true,
+      showSorterTooltip: false,
+      render: (createdAt: Date) => moment(createdAt).format(FORMAT_DATE),
+    },
+    {
+      title: 'Opciones',
+      fixed: 'right',
+      dataIndex: '_id',
+      align: 'center',
+      render: () => {
+        return (
+          <Space>
+            <Tooltip title="Imprimir Devolución">
+              <Button type="primary" onClick={() => {}} icon={<PrinterFilled />} />
+            </Tooltip>
+            <Space>
+              <Tooltip title="Imprimir Cupon">
+                <Button type="ghost" onClick={() => {}} icon={<PrinterFilled />} />
+              </Tooltip>
+            </Space>
+          </Space>
+        );
+      },
+    },
+  ];
+
   return (
     <PageContainer>
-      <Card>
+      <Card bordered={false}>
         <Form>
-          <Row gutter={[10, 0]}>
-            <Col span={5}>
+          <Row gutter={[15, 0]}>
+            <Col xs={24} md={7} lg={6}>
               <FormItem label="Código Pedido">
                 <Input />
               </FormItem>
             </Col>
-            <Col span={5}>
+            <Col xs={24} md={7} lg={6}>
               <FormItem label="Código Cupón">
                 <Input />
               </FormItem>
             </Col>
-            <Col span={5}>
+            <Col xs={24} md={7} lg={6}>
               <FormItem label="Código Factura">
                 <Input />
               </FormItem>
             </Col>
-            <Col span={7}>
+            <Col xs={24} md={7} lg={6}>
               <FormItem label="Tienda">
                 <Select showSearch placeholder="Seleccione una tienda">
                   <Option value="Gucci">Gucci</Option>
@@ -36,7 +141,7 @@ const ReturnList = () => {
                 </Select>
               </FormItem>
             </Col>
-            <Col span={4}>
+            <Col xs={24} md={7} lg={4}>
               <FormItem>
                 <Space>
                   <Button icon={<SearchOutlined />} type="primary" htmlType="submit">
@@ -50,23 +155,32 @@ const ReturnList = () => {
             </Col>
           </Row>
         </Form>
-        <Divider style={{ margin: 0 }} />
+        <Divider style={styles.dividerNoMargin} />
         <Row>
-          <Col span={18}>
-            <Button icon={<PlusOutlined />} shape="round" color="primary">
+          <Col xs={12} md={15} lg={15}>
+            <Button
+              onClick={() => setVisible(true)}
+              icon={<PlusOutlined />}
+              shape="round"
+              type="primary"
+            >
               Nuevo
             </Button>
           </Col>
-          <Col span={6}>
+          <Col xs={12} md={9} lg={9}>
             <Space>
               <Text strong>Total Encontrados:</Text>
-              <Text>{10}</Text>
-              <Text strong>Pagina</Text>
+              <Text>10</Text>
+              <Text strong>Pagina:</Text>
               <Text>1/1</Text>
             </Space>
           </Col>
         </Row>
       </Card>
+      <Card bordered={false} bodyStyle={styles.noPaddingTop}>
+        <Table columns={columns} scroll={{ x: 1000 }} pagination={false} dataSource={dataTest} />
+      </Card>
+      <FormReturn visible={visible} onCancel={closeModal} />
     </PageContainer>
   );
 };
