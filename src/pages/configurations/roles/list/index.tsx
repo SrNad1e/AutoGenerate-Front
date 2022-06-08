5c32e17f6a095b75da16fd73e59e7a87009a7b57
Camilo <camilob290421@gmail.com>
2022-06-08 16:30:42 -0500

    💥 feat(compiler): add se inicio interfaz de roles

--- src/pages/configurations/roles/list/index.tsx
+++ src/pages/configurations/roles/list/index.tsx
@@ -1,21 +1,59 @@
+/*import { PlusOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Form, Input } from 'antd';
+import { Button, Card, Col, Form, Input, Row, Table } from 'antd';
 
 const { Search } = Input;
 const FormItem = Form.Item;
 
 const RolesList = () => {
+  const column = [
+    {
+      title: 'Nombre',
+    },
+    {
+      title: 'Descripción',
+    },
+    {
+      title: 'Activo',
+    },
+    {
+      title: 'Opciones',
+    },
+  ];
+/*
   return (
     <PageContainer>
       <Card>
-        <Form layout="inline">
+        <Form layout="horizontal">
           <FormItem label="Nombre" name="name">
             <Search placeholder="Nombre del rol" enterButton />
           </FormItem>
         </Form>
+        <Row gutter={[0, 20]}>
+          <Col span={12}>
+            <Button
+              disabled={false}
+              icon={<PlusOutlined />}
+              type="primary"
+              shape="round"
+              onClick={() => visibleModal(color)}
+            >
+              Nuevo
+            </Button>
+          </Col>
+          <Col span={12} className={styles.alignRigth}>
+            <Text strong>Total Encontrados:</Text> {data?.colors.totalDocs}{' '}
+            <Text strong>Páginas: </Text> {data?.colors.page} / {data?.colors.totalPages || 0}
+          </Col>
+          <Col span={24}>
+            <Table columns={column} scroll={{ x: 'auto' }} />
+          </Col>
+        </Row>
       </Card>
     </PageContainer>
-  );
-};
+  );*
+};/
 
-export default RolesList;
+export default RolesList;*/
+
+export default () => <></>;
