5549ac488350cce6ec8772a32016e6a80a0f781f
Manuel Rodríguez <manuel.rodriguez@the-cocktail.com>
2022-09-19 12:57:51 -0500

    🐛 fix(compiler): Se agrega _id al iniciar la interface

--- config/config.pre.ts
+++ config/config.pre.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     'react-dev-inspector/plugins/umi/react-inspector',
   ],
   define: {
-    API_URL: 'http://localhost:8080/graphql',
+    API_URL: 'http://localhost:8080',
     CDN_URL: 'https://toulouse-storage.s3.amazonaws.com',
     COMPANY_ID: '62503250f035862fa3925f70',
     FORMAT_DATE: 'DD/MM/YYYY HH:mm:ss',
