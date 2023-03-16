3d087f8b4d6615b2f0b57118a630ab65b655dbae
Nobody <gerencia@luckywoman.com.co>
2023-03-16 09:13:31 -0500

    💥 feat(compiler): add se agrega formato de impresion de cierre verificado en la interfaz de cierre z

---
+++ src/pages/invoicing/closings/closingZ/reports/closeVerified.js
@@ -0,0 +1,487 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import moment from 'moment';
+import numeral from 'numeral';
+
+const classes = {
+  content: {
+    margin: 20,
+    maxWidth: '60mm',
+    color: 'black',
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'black',
+  },
+  text: {
+    fontSize: 12,
+    width: '100%',
+    color: 'black',
+  },
+  textBold: {
+    fontWeight: 'bold',
+    marginRight: 10,
+    color: 'black',
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: '100%',
+    marginTop: 10,
+    color: 'black',
+  },
+  title1: {
+    fontWeight: 500,
+    fontSize: 13,
+    textAlign: 'center',
+    width: '100%',
+    marginTop: 10,
+    color: 'black',
+  },
+  row: {
+    display: 'flex',
+    color: 'black',
+  },
+  col1: {
+    width: '100%',
+    textAlign: 'left',
+    color: 'black',
+  },
+  col2: {
+    width: '50%',
+    textAlign: 'right',
+    color: 'black',
+  },
+  body: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: 10,
+    color: 'black',
+  },
+};
+
+export default class ReportCloseV extends React.PureComponent {
+  render() {
+    const { data } = this.props;
+
+    const totalPayments = data?.payments?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type !== 'CASH' ? payment?.value : 0),
+      0,
+    );
+
+    const totalBank = data?.payments?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'BANK' ? payment?.value : 0),
+      0,
+    );
+
+    const quantityBank = data?.payments?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'BANK' ? payment?.quantity : 0),
+      0,
+    );
+
+    const totalBonus = data?.payments?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'BONUS' ? payment?.value : 0),
+      0,
+    );
+
+    const quantityBonus = data?.payments?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'BONUS' ? payment?.quantity : 0),
+      0,
+    );
+
+    const totalCredit = data?.payments?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'CREDIT' ? payment?.value : 0),
+      0,
+    );
+
+    const quantityDataphone = data?.payments?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'DATAPHONE' ? payment?.quantity : 0),
+      0,
+    );
+
+    const cashRegister = { ...data?.cashRegister };
+    if (cashRegister) {
+      delete cashRegister['__typename'];
+    }
+
+    const quantityCreditBank = data?.paymentsCredit?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'BANK' ? payment?.quantity : 0),
+      0,
+    );
+
+    const paymentCreditBank = data?.paymentsCredit?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type === 'BANK' ? payment?.value : 0),
+      0,
+    );
+
+    const getTotalDataphone = () => {
+      return (
+        data?.payments?.reduce(
+          (sum, payment) => sum + (payment?.payment?.type === 'DATAPHONE' ? payment?.value : 0),
+          0,
+        ) || 0
+      );
+    };
+
+    const diffDataphone = data?.newQuantityDataphone - quantityDataphone;
+
+    const totalCreditPayments = data?.paymentsCredit?.reduce(
+      (sum, payment) => sum + (payment?.payment?.type !== 'CASH' ? payment?.value : 0),
+      0,
+    );
+
+    const creditPayments = data?.paymentsCredit?.reduce((sum, payment) => sum + payment?.value, 0);
+
+    const diff = data?.newTotalPaymentReport + data?.newExpense - data?.newTotalPaymentRegister;
+    const diffBank = data?.newQuantityBank - ((quantityBank || 0) + quantityCreditBank);
+
+    return (
+      <div style={classes.content}>
+        <div style={classes.header}>
+          <img src="/logo.svg" alt="logo" width="50%" style={{ marginBottom: -15 }} />
+          <div style={{ ...classes.title, marginBottom: 5 }}>CIERRE VERIFICADO</div>
+          <div style={classes.text}>
+            <div style={classes.row}>
+              <div style={classes.col1}>
+                <span style={{ fontWeight: 'bold' }}>CIERRE # </span>
+                {data?.prefix} {data?.number}
+              </div>
+              <div style={classes.col2}>
+                <span style={{ fontWeight: 'bold' }}>DIA </span>{' '}
+                {moment(data?.closeDate).utc().format('DD/MM/YYYY')}
+              </div>
+            </div>
+          </div>
+          <div style={classes.text}>Registrado: {moment(data?.createdAt).format('DD/MM/YYYY')}</div>
+          <div style={{ width: '100%', lineHeight: 1.5 }}>
+            <div style={classes.text}>
+              <span style={classes.textBold}>Tienda:</span>
+              {data?.pointOfSale?.shop?.name}
+            </div>
+          </div>
+          <div style={{ width: '100%', lineHeight: 1.5 }}>
+            <div style={classes.text}>
+              <span style={classes.textBold}>Usuario:</span>
+              {data?.user?.name}
+            </div>
+          </div>
+          <div style={classes.body}>
+            <div style={classes.title}>
+              <div style={classes.row}>
+                <div>Resumen del día</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Pedidos finalizados:</div>
+                <div style={classes.col2}>{data?.summaryOrder?.quantityClosed}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Pedidos cancelados:</div>
+                <div style={classes.col2}>{data?.summaryOrder?.quantityCancel}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Pedidos pendientes:</div>
+                <div style={classes.col2}>{data?.summaryOrder?.quantityOpen}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Valor Facturas:</div>
+                <div style={classes.col2}>{numeral(data?.summaryOrder?.value).format('$ 0,0')}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Total Pagos:</div>
+                <div style={classes.col2}>
+                  {numeral(
+                    totalPayments + totalCreditPayments + data?.newTotalPaymentRegister,
+                  ).format('$ 0,0')}
+                </div>
+              </div>
+            </div>
+            <div style={classes.title}>
+              <div style={classes.row}>
+                <div>Cupones Redimidos</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Cantidad:</div>
+                <div style={classes.col2}>{quantityBonus}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}> Valor:</div>
+                <div style={classes.col2}>{numeral(totalBonus).format('$ 0,0')}</div>
+              </div>
+            </div>
+            <div style={classes.title}>
+              <div style={classes.row}>
+                <div>Devoluciones</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Valor:</div>
+                <div style={classes.col2}>{numeral(data?.refunds?.value || 0).format('$ 0,0')}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Cantidad de Productos:</div>
+                <div style={classes.col2}>{data?.refunds?.quantity || 0}</div>
+              </div>
+            </div>
+            <div style={classes.title}>
+              <div style={classes.row}>
+                <div>Efectivo</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Registrado:</div>
+                <div style={classes.col2}>
+                  {numeral(data?.newTotalPaymentRegister).format('$ 0,0')}
+                </div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Reportado:</div>
+                <div style={classes.col2}>
+                  {numeral(data?.newTotalPaymentReport).format('$ 0,0')}
+                </div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Egresos:</div>
+                <div style={classes.col2}>{numeral(data?.newExpense).format('$ 0,0')}</div>
+              </div>
+            </div>
+            {diff !== 0 && (
+              <div style={classes.text}>
+                <div style={classes.row}>
+                  <div style={classes.col1}>{diff > 0 ? 'Sobrante' : 'Faltante'}:</div>
+                  <div style={classes.col2}>
+                    {numeral(diff > 0 ? diff : diff * -1).format('$ 0,0')}
+                  </div>
+                </div>
+              </div>
+            )}
+            <div style={classes.title}>
+              <div style={classes.row}>
+                <div>Consignaciones</div>
+              </div>
+            </div>
+            <div style={classes.title1}>
+              <div style={classes.row}>
+                <div>Datáfono</div>
+              </div>
+            </div>
+            <>
+              {' '}
+              <div style={classes.text}>
+                <div style={classes.row}>
+                  <div style={classes.col1}>C. Datáfono Registrado:</div>
+                  <div style={classes.col2}>{quantityDataphone}</div>
+                </div>
+              </div>
+              <div style={classes.text}>
+                <div style={classes.row}>
+                  <div style={classes.col1}>Total Datáfono Registrado:</div>
+                  <div style={classes.col2}>{numeral(getTotalDataphone()).format('$ 0,0')}</div>
+                </div>
+              </div>
+              <div style={classes.text}>
+                <div style={classes.row}>
+                  <div style={classes.col1}>C. Datáfono Reportado:</div>
+                  <div style={classes.col2}>{data?.newQuantityDataphone || 0}</div>
+                </div>
+              </div>
+              {diffDataphone !== 0 && (
+                <div style={classes.text}>
+                  <div style={classes.row}>
+                    <div style={classes.col1}>
+                      {diffDataphone > 0 ? 'Sobrante Datáfono' : 'Faltante Datáfono'}:
+                    </div>
+                    <div style={classes.col2}>
+                      {diffDataphone > 0 ? diffDataphone : diffDataphone * -1}
+                    </div>
+                  </div>
+                </div>
+              )}{' '}
+            </>
+            <div style={classes.title1}>
+              <div style={classes.row}>
+                <div>Bancolombia</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>C. Bancolombia Registrado:</div>
+                <div style={classes.col2}>{quantityBank + quantityCreditBank}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>T. Bancolombia Registrado:</div>
+                <div style={classes.col2}>
+                  {numeral(totalBank + paymentCreditBank).format('$ 0,0')}
+                </div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>C. Bancolombia Reportado:</div>
+                <div style={classes.col2}>{data?.newQuantityBank}</div>
+              </div>
+            </div>
+            {diffBank !== 0 && (
+              <div style={classes.text}>
+                <div style={classes.row}>
+                  <div style={classes.col1}>{diffBank > 0 ? 'Sobrante' : 'Faltante'}:</div>
+                  <div style={classes.col2}>{diffBank > 0 ? diffBank : diffBank * -1}</div>
+                </div>
+              </div>
+            )}
+            <div style={classes.title}>
+              <div style={classes.row}>
+                <div>Detalle pagos</div>
+              </div>
+            </div>
+            {data?.paymentsCredit?.length < data?.payment?.length
+              ? data?.payments?.map(({ payment, value }) => (
+                  <div key={payment?._id} style={classes.text}>
+                    <div style={classes.row}>
+                      <div style={classes.col1}>{payment?.name}:</div>
+                      {payment.type === 'BANK' ? (
+                        <div style={classes.col2}>
+                          {' '}
+                          {numeral(value + paymentCreditBank).format('$ 0,0')}
+                        </div>
+                      ) : payment.type === 'CASH' ? (
+                        <div style={classes.col2}>
+                          {' '}
+                          {numeral(data?.newTotalPaymentRegister).format('$ 0,0')}
+                        </div>
+                      ) : (
+                        <div style={classes.col2}> {numeral(value).format('$ 0,0')}</div>
+                      )}
+                    </div>
+                  </div>
+                ))
+              : data?.paymentsCredit?.map(({ payment, value }) => (
+                  <div key={payment?._id} style={classes.text}>
+                    <div style={classes.row}>
+                      <div style={classes.col1}>{payment?.name}:</div>
+                      {payment.type === 'BANK' ? (
+                        <div style={classes.col2}>
+                          {' '}
+                          {numeral(value + totalBank).format('$ 0,0')}
+                        </div>
+                      ) : payment.type === 'CASH' ? (
+                        <div style={classes.col2}>
+                          {' '}
+                          {numeral(data?.newTotalPaymentRegister).format('$ 0,0')}
+                        </div>
+                      ) : (
+                        <div style={classes.col2}> {numeral(value).format('$ 0,0')}</div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Total Pagos:</div>
+                <div style={classes.col2}>
+                  {numeral(
+                    totalPayments + totalCreditPayments + data?.newTotalPaymentRegister,
+                  ).format('$ 0,0')}
+                </div>
+              </div>
+            </div>
+            <div style={classes.title}>
+              <div style={classes.row}>
+                <div>Detalle créditos</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Ventas a Crédito:</div>
+                <div style={classes.col2}> {numeral(totalCredit).format('$ 0,0')}</div>
+              </div>
+            </div>
+            <div style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>Abonos a Crédito:</div>
+                <div style={classes.col2}> {numeral(creditPayments).format('$ 0,0')}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style={classes.header}>
+          <img src="/logo.svg" alt="logo" width="50%" style={{ marginBottom: -15 }} />
+          <div style={{ ...classes.title, marginBottom: 5 }}>ARQUEO</div>
+          <div style={classes.text}>
+            Fecha Registro: {moment(data?.createdAt).format('DD/MM/YYYY HH:mm:ss')}
+          </div>
+          <div style={{ width: '100%', lineHeight: 1.5 }}>
+            <div style={classes.text}>
+              <span style={classes.textBold}>Cierre No.</span>
+              {data?.prefix} {data?.number}
+            </div>
+          </div>
+          <div style={{ width: '100%', lineHeight: 1.5 }}>
+            <div style={classes.text}>
+              <span style={classes.textBold}>Tienda:</span>
+              {data?.pointOfSale?.shop?.name}
+            </div>
+          </div>
+          <div style={{ width: '100%', lineHeight: 1.5 }}>
+            <div style={classes.text}>
+              <span style={classes.textBold}>Usuario:</span>
+              {data?.user?.name}
+            </div>
+          </div>
+        </div>
+        <div style={classes.body}>
+          <div style={classes.title}>Lista de denominaciones</div>
+          {Object.keys(cashRegister || {}).map((key) => (
+            <div key={key} style={classes.text}>
+              <div style={classes.row}>
+                <div style={classes.col1}>{key}</div>
+                <div style={classes.col2}>{cashRegister[key]}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div style={{ ...classes.text, fontWeight: 'bold', marginTop: 15, fontSize: 20 }}>
+          <div style={classes.row}>
+            <div style={classes.col1}>Total:</div>
+            <div style={classes.col2}>
+              {numeral(
+                Object.keys(cashRegister || {}).reduce(
+                  (sum, key) => sum + parseInt(cashRegister[key] * parseInt(key.slice(1)) || 0),
+                  0,
+                ),
+              ).format('$ 0,0')}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
